07d1d52516dae5e2a68da3c75d123197c78a326e
vivinkv6 <vivinkv0@gmail.com>
2022-05-26 08:17:23 +0530

    modify score.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -8,8 +8,8 @@ function Score({ bmi, bmiType,weightMeasure }) {
     <div className="score border border-dark mt-3 px-4 py-5" style={{ boxShadow: '0px 0px 10px black' }} >
       <h5 className="text-center mt-2 text-secondary">BMI Score</h5>
       <div className='score mt-2'>
-        <div className='mt-3 ms-5 me-5' style={{padding:'10px 20px', backgroundColor: 'rgb(132, 251, 255)', textAlign: 'center' }}>
-          <h2 style={{display:'inline', fontSize: '50px'}} >{bmi}</h2>
+        <div className='mt-3 ' style={{display:'flex', justifyContent:'center'}}>
+          <h2 style={{display:'inline',backgroundColor: 'rgb(132, 251, 255)',padding:'10px 10px 10px 10px', fontSize: '50px',fontWeight:'bolder'}} >{bmi}</h2>
         </div>
         <h3 className='text-center text-primary mt-2 ' style={{ fontWeight: 'bold', fontSize: '35px' }}>{bmiType}</h3>
       </div>
